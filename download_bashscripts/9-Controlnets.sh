39d0e5f7b2698da4c2bb61573c6d3926897143c4
Theodore <tinprocoder0908@gmail.com>
2024-07-27 00:41:28 +0700

    update code

--- download_bashscripts/9-Controlnets.sh
+++ download_bashscripts/9-Controlnets.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-git clone https://huggingface.co/lllyasviel/control_v11p_sd15_softedge models/Controlnet/control_v11p_sd15_softedge
-git clone https://huggingface.co/lllyasviel/control_v11f1p_sd15_depth models/Controlnet/control_v11f1p_sd15_depth
-git clone https://huggingface.co/lllyasviel/control_v11p_sd15_canny models/Controlnet/control_v11p_sd15_canny
+git clone https://huggingface.co/lllyasviel/control_v11p_sd15_openpose models/Controlnet/control_v11p_sd15_openpose
